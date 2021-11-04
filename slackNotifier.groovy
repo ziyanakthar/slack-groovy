b49465dccf7e6a09454200279706e9aa89f10652
ziyanakthar <38223075+ziyanakthar@users.noreply.github.com>
2021-11-04 13:50:40 +0530

    Adding the logic to capture commit history

--- slackNotifier.groovy
+++ slackNotifier.groovy
@@ -31,3 +31,23 @@ node {
         notifySlack(currentBuild.result)
     }
 }
+
+
+post {
+    failure {
+        slackSend (color: '#FF0000', message: """FAILED:
+Job: ${env.JOB_NAME}
+Build #${env.BUILD_NUMBER}
+Build: ${env.BUILD_URL})
+Comitted by: ${env.GIT_AUTHOR}
+Last commit message: '${env.GIT_COMMIT_MSG}'""")
+    }
+    success {
+        slackSend (color: '#00FF00', message: """SUCCESS:
+Job: ${env.JOB_NAME}
+Build #${env.BUILD_NUMBER}
+Build: ${env.BUILD_URL})
+Comitted by: ${env.GIT_AUTHOR}
+Last commit message: '${env.GIT_COMMIT_MSG}'""")
+    }
+  }
